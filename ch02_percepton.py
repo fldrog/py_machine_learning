3025cbd42fa0ec5e4b50978d6edbf96abb84c09c
Florin Botis <Florin.Botis@esl-asia.com>
2016-06-29 17:48:50 +0200

    Iris learning

--- ch02_percepton.py
+++ ch02_percepton.py
@@ -49,7 +49,6 @@ class Perceptron(object):
                 self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
-                print self.w_
             self.errors_.append(errors)
         return self
 
@@ -80,5 +79,70 @@ trainint_results=np.array([1,1,1,1,-1,-1,-1,-1,1,1,1,1])
 p.fit(training,trainint_results)
 print(p.predict(np.array([1,8])))
 print(p.predict(np.array([8,1])))
+
+
+
+
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+print(df.tail())
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+
+# STEP 1
+# plt.scatter(X[:50, 0], X[:50, 1],color='red', marker='o', label='setosa')
+# plt.scatter(X[50:100, 0], X[50:100, 1],color='blue', marker='x', label='versicolor')
+# plt.xlabel('sepal length')
+# plt.ylabel('petal length')
+# plt.legend(loc='upper left')
+
+
+# STEP 2
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+# plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+# plt.xlabel('Epochs')
+# plt.ylabel('Number of misclassifications')
+# plt.show()
+
+# STEP 3
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                         np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
 
 
